1487b64a3d8df3dbbdba447d03c55423e6e51272
lion_good <lion_good_very@hotmail.com>
2016-11-21 00:18:02 +0800

    add menu

--- app/src/main/java/org/lion/together/ui/GistFragment.java
+++ app/src/main/java/org/lion/together/ui/GistFragment.java
@@ -10,7 +10,6 @@ import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import com.blankj.utilcode.utils.SPUtils;
